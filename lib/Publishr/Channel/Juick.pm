5c9b32510d6f2c5c3864489a9a612cf73c4f3f7f
Sergey Romanov <sromanov-dev@yandex.ru>
2014-11-30 15:10:23 +0300

    add juick (why? because we can!)

---
+++ lib/Publishr/Channel/Juick.pm
@@ -0,0 +1,56 @@
+package Publishr::Channel::Juick;
+
+use strict;
+use warnings;
+
+use Net::XMPP::Client::GTalk;
+use Publishr;
+
+my $TARGET_JID = 'juick@juick.com';
+
+sub new {
+    my $class = shift;
+    my (%params) = @_;
+
+    my $self = {};
+    bless $self, $class;
+
+    $self->{username} = $params{jid};
+    $self->{password} = $params{password};
+
+    return $self;
+}
+
+sub publish {
+    my $self = shift;
+    my ($message) = @_;
+
+    my $client = $self->build_agent;
+
+    my $tag_line = join ' ', map { "*$_" } split /,\s*/, $message->{tags};
+    my $res = $client->send_message($TARGET_JID, <<"MESSAGE");
+$tag_line
+$message->{status}
+
+$message->{text}
+
+$message->{link}
+MESSAGE
+
+    return $self;
+}
+
+sub build_agent {
+    my $self = shift;
+
+    # only Google Talk registered JIDs supported for now
+    $self->{username} =~ s/(\@.*)//;
+
+    return Net::XMPP::Client::GTalk->new(
+        USERNAME => $self->{username},
+        PASSWORD => $self->{password},
+        RESOURCE => "Publishr/$Publishr::VERSION",
+    );
+}
+
+1;
